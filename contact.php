28b47a0139c6c11edd87068131bbfa7c59045821
Amar1921 <57800962+Amar1921@users.noreply.github.com>
2020-08-20 22:18:30 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -12,10 +12,12 @@ if (isset($_GET)){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/style/main.css">
+
     <title>Contact</title>
 </head>
 <body>
